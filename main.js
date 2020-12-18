b42bc5d6ff9ab6107128743b8fa25387a853f3b1
Timothy Do <tydo83@gmail.com>
2020-12-18 14:56:32 -0500

    added remove and clear dom

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
-//memo part
+//memo section
 const memoBtn = document.querySelector(".add-btn");
-const removeBtn = document.querySelector(".remove-btn");
-const clearBtn = document.querySelector(".clear-btn")
-
 memoBtn.addEventListener('click', () => {
     const memoItem = document.createElement('li');
     const memoDisplay = document.querySelector('.memo-item')
-    const memo = document.querySelector('.memo-input')
-    memoItem.innerText = memo.value 
+    const memoUserInput = document.querySelector('.memo-input')
+    memoItem.innerText = memoUserInput.value
+    memoItem.id = memoItem.length
     memoDisplay.appendChild(memoItem);
 })
 
+const removeBtn = document.querySelector(".remove-btn");
 removeBtn.addEventListener('click', () => {
-    
+    const lists = document.querySelectorAll('li');
+    for (const item of lists) {
+        item.addEventListener('click', (event) => {
+            event.target.remove();
+        })
+    }
 })
 
+const clearBtn = document.querySelector(".clear-btn")
+clearBtn.addEventListener('click', () => {
+    const memoItem = document.querySelector('.memo-item');
+    memoItem.innerText = "";
+})
 
 // search part
 const xhr = new XMLHttpRequest();
@@ -30,11 +39,10 @@ xhr.addEventListener('loadend', () => {
 
 const searchBtn = document.querySelector('.search-button')
 searchBtn.addEventListener('click', () => {
-    
     const userInput = JSON.stringify({
         query: document.querySelector(".search-box").value,
     })
-    
+
     xhr.open("POST", "https://trackapi.nutritionix.com/v2/natural/nutrients");
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.setRequestHeader("x-app-id", app_id);
